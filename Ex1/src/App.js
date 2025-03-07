1898b68ea735041392cce4d0b60d3143a80d1c50
Akila Fernando <144292336+Akila-Fernando@users.noreply.github.com>
2025-03-07 12:36:42 -0700

    Ex3 done

---
+++ Ex1/src/App.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const date = new Date();
+  const currentyear = date.getFullYear();
+  const isLoggedIn = false;
+  return (
+    <div>
+        <h1>ENSF-381: Full Stack Web Development</h1>
+        <p>React Components</p>
+        <p>{currentyear}</p>
+        {isLoggedIn === true? "Welcome Back!" : "Please Log in"}
+    </div>
+  );
+}
+
+
+
+
+export default App;
--- Ex2/src/App.js
+++ Ex2/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Home from './Home';
 import Contact from './Contact';
 import About from './About';
-import EngineeringTopics from './EngineeringTopics'
 
 function App() {
   const date = new Date();
@@ -15,12 +14,9 @@ function App() {
         <p>React Components</p>
         <p>{currentyear}</p>
         {isLoggedIn === true? "Welcome Back!" : "Please Log in"}
-        
         <Home title="Home Page" description="Welcome to our website."/>
         <About title="About Us" description="We are passionate about delivering quality experiences."/>
         <Contact title="Contact Us" description="Feel free to reach out to us via email or phone."/>
-
-        <EngineeringTopics />
     </div>
   );
 
