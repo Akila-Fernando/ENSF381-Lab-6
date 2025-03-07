2948cdd5e4b613fce750bfbdaa7874f5b12e851c
tanvimahal <tanvii.mahal@gmail.com>
2025-03-07 11:46:00 -0700

    Update App.js

--- jsx-exercise/src/App.js
+++ jsx-exercise/src/App.js
@@ -2,13 +2,12 @@
 import './App.css';
 
 function App() {
-
   const date = new Date();
   const currentyear = date.getFullYear();
   const isLoggedIn = false;
   return (
     <div>
-        <h1>ENSF-381: Full Stack Web Development</h1>;
+        <h1>ENSF-381: Full Stack Web Development</h1>
         <p>React Components</p>
         <p>{currentyear}</p>
         {isLoggedIn === true? "Welcome Back!" : "Please Log in"}
